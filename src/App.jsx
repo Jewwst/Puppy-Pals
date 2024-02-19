43e86bd4eccc881e7816c79e19a38a43cd3bec94
Jewwst <pegasus_hurcules@yahoo.com>
2024-02-19 13:51:26 -0600

    finished but cant get the featured puppy to show in console log

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,45 @@
-
+import { useState } from 'react'
+import { puppyList } from './data.js'
+import './index.css'
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null);
+  const [featuredPup2, setfP] = useState(null);
+  console.log(puppies)
 
-  
-    <>
-      <div>
-        
+  function handleClick() {
+    
+  }
+
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup)
+
+  return (
+    
+      <div className="App">
+
+      {puppies.map((puppy) => {
+        return (
+        <p onClick={()=>{ setFeatPupId(puppy.id)}} key={puppy.id}>
+        <div>{puppy.name}</div>
+        </p>
+        );
+      })}
+
+        {featPupId && (
+        <div className="Puppy">
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+
       </div>
-    </>
-  
+      
+);
 }
 
 export default App
